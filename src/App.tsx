e5163ee5557730600f313bbd70043cd27a277973
kutmanhack <topchubaev.kutman@gmail.com>
2023-10-22 12:46:49 +0600

    kutmsn00

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 import Header from "./components/Header/header";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/home";
 import Footer from "./components/Footer/footer";
 import AboutUs from "./components/AboutUs/aboutUs";
 import Categories from "./components/Categories/categories";
+import Sale from "./pages.d.ts/Sale/Sale";
 
 function App() {
   return (
     <div className="App">
-    <Header/>
+      <Header />
       <Routes>
-        <Route path={"/"} element={<Home/>}/>
-        <Route path={"/о нас"} element={<AboutUs/>}/>
-        <Route path={"/категории"} element={<Categories/>}/>
-        <Route path={"/покупка/продажа"} element={<Home/>}/>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/о нас"} element={<AboutUs />} />
+        <Route path={"/категории"} element={<Categories />} />
+        <Route path={"/покупка/продажа"} element={<Sale />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
